b9463ed645f786e9d58faa544142b8987e89d417
Ilva <ilva.paidere@gmail.com>
2018-09-18 18:02:12 +0300

    Initial commit

---
+++ Uzdevums1/src/Uzdevums1.java
@@ -0,0 +1,15 @@
+
+public class Uzdevums1 {
+
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 6;
+		int sum = x + y;
+		System.out.println(sum);
+
+	}
+
+
+	
+
+}
---
+++ Uzdevums1/src/Uzdevums2.java
@@ -0,0 +1,10 @@
+
+public class Uzdevums2 {
+	public static void main(String[] args) {
+		int x = 3;
+		int y = 5;
+		int z = 6;
+		System.out.println((x + y + z) /3);
+	}
+
+}
